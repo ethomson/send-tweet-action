e429d18398f1bc6c50f28c207c21d64c1a40870c
Felix Kratzer <fez@Felixs-MBP.fritz.box>
2020-04-15 09:33:17 +0200

    Status now accepts an array of statuses as input

--- src/main.ts
+++ src/main.ts
@@ -20,16 +20,21 @@ async function run(): Promise<void> {
       access_token_secret: core.getInput('access-token-secret')
     })
 
-    twitter.post(
-      '/statuses/update',
-      {status: core.getInput('status')},
-      (error, data, response) => {
-        if (error) throw error
+    const statusCandidate = core.getInput('status')
+    let status = statusCandidate // If in doubt assume it's a string
 
-        console.log(data)
-        console.log(response)
-      }
-    )
+    if (Array.isArray(statusCandidate)) {
+      // In case it is an array, select the status using the current date (day of month)
+      const idx = new Date().getDate() % statusCandidate.length
+      status = statusCandidate[idx]
+    }
+
+    twitter.post('/statuses/update', {status}, (error, data, response) => {
+      if (error) throw error
+
+      console.log(data)
+      console.log(response)
+    })
   } catch (error) {
     core.setFailed(error.message)
   }
